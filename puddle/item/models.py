fce54963eec0e27083a75723923ddb512febedbf
ismoil27 <abduraimovismoil98@gmail.com>
2023-12-20 10:33:01 +0900

    feat: developing detail page for products

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    decription=models.TextField(blank=True, null=True)
+    description=models.TextField(blank=True, null=True)
     price = models.FloatField()
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField()
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -6,6 +6,7 @@ from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('item/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path("admin/", admin.site.urls),
 ]
