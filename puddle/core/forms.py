b6e932245279f592e7f8531e8e862fcfc02d96a7
ismoil27 <abduraimovismoil98@gmail.com>
2023-12-20 14:44:32 +0900

    feat: develop login page and logged user logic

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -1,13 +1,23 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'w-full py-4 px-4 rounded-xl'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'w-full py-4 px-4 rounded-xl'
+    }))
 
 class SignupForm(UserCreationForm):
     class Meta: 
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        
+
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Your username',
         'class': 'w-full py-4 px-4 rounded-xl'
