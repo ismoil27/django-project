53ad8f8732d4a81ee080030a032b5d072bac987f
ismoil27 <abduraimovismoil98@gmail.com>
2023-12-21 17:37:31 +0900

    feat: conversation logic development

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('item/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
     path("admin/", admin.site.urls),
 ]
 
